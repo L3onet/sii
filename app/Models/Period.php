41b83ebe0b0434c6d6b62e140c26fabca69c1198
Leonel Gonzalez Vidales <leonelgv@live.com>
2023-10-23 08:13:29 -0600

    Repositorio del sistema SII. Proyecto inicial.

---
+++ app/Models/Period.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Period extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'short_name',
+        'long_name',
+        'start_date',
+        'final_date',
+        'status',
+    ];
+}
